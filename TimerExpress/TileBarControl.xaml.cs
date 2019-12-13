69a041186328b7b7fcff43e34267987cfad5d5b6
Boris Balaban <BorisBalaban71@gmail.com>
2019-12-14 00:32:01 +0300

    Add custom tile bar

---
+++ TimerExpress/TileBarControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimerExpress {
+    /// <summary>
+    /// Interaction logic for TileBarControl.xaml
+    /// </summary>
+    public partial class TileBarControl : UserControl {
+
+        public TileBarControl() {
+            this.DataContext = this;
+
+            InitializeComponent();
+        }
+
+        private void Hide_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e) {
+
+        }
+ 
+    }
+}
